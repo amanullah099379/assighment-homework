41348f2fe2d30cd566e2ba2689bc7d4eec51efdf
islam0317 <155236290+islam0317@users.noreply.github.com>
2024-02-29 04:37:15 -0800

    ssss

---
+++ chap 2/1app.js
@@ -0,0 +1,14 @@
+        var username = 'sheikh islam'
+        var age = '17'
+        var education = 'matriculation in karachi board'
+        var gmail = 'sheikhislam9871@gmail.com' 
+        var book = 'iam trying to learn from the book a smarter way to learn javascript' 
+
+
+
+alert(username)
+alert(age)
+alert(education)
+alert("PIZZA \nPIZZ \nPIZ \nPI \nP")
+alert(gmail)
+alert(book)
